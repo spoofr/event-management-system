d3fb873327ad5717e24d17b7208013df26ba97ca
sinyadal <v0xe7y2cl@protonmail.com>
2018-03-08 16:56:14 +0800

    Update navbar logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
             <div class="container">
-                <a href="{{ url('/') }}" class="navbar-brand">Favapp</a>
+                <a href="{{ url('/') }}" class="navbar-brand">EasyCalendar</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown"
                     aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
