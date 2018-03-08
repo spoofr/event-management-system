43090eb68e7dc5e90306227163097d5595f8fc64
sinyadal <v0xe7y2cl@protonmail.com>
2018-03-08 16:55:02 +0800

    Remove calendar route

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/calendar', function () {
-    return view('calendar');
-})->name('calendar');
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
