4788b7cfbee1a210dbc50e3694fdc9734359da6c
sinyadal <v0xe7y2cl@protonmail.com>
2018-03-24 22:17:46 +0800

    Add event name in modal

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -182,7 +182,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">
-                        <span id="name"></span>
+                        Event Name: <span id="name"></span>
                     </h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
