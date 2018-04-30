b05b0ba406281c7ba855361218b56b76b35b0ca4
kaitoAckerman <k4170.4ck3rm4n@gmail.com>
2018-05-01 00:08:44 +0800

    Add api function
    move api.php code into api function with $zon data

--- app/Http/Controllers/SolatController.php
+++ app/Http/Controllers/SolatController.php
@@ -6,7 +6,45 @@ use Illuminate\Http\Request;
 
 class SolatController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('solat.index');
+    }
+    
+    public function api($zon)
+    {
+        // if (isset($_GET['zon'])) {
+            $kodzon = $zon; # store get parameter in variable
+            $xmlurl = "http://www2.e-solat.gov.my/xml/today/index.php?zon=".$kodzon; # url of JAKIM eSolat XML data
+            
+            # fetch xml file (from JAKIM website)
+        # parse xml data into object
+        $data = simplexml_load_file($xmlurl);
+            
+        # access xml data, get name of zone, trim object
+        $namazon = trim($data->channel[0]->link);
+        $tarikhmasa = trim($data->channel[0]->children('dc', true)->date); # use children() to access tag with colon (:)
+            
+        # create associative array to store waktu solat
+        $arrwaktu = array();
+        
+        # iterate through the "item" tag in the xml, access, and store into array
+        foreach ($data->channel[0]->item as $item) {
+            # access data and trims object, to store as string
+                $solat = "waktu_" . strtolower(trim($item->title)); # append "waktu_" to the lowercase string. ex: "waktu_subuh"
+                $waktu = trim($item->description);
+                
+            # store into associative array
+            $arrwaktu[$solat] = $waktu;
+        }
+        
+        # add kod_zon, nama_zon, and tarikh_masa to the array
+        $arrwaktu["kod_zon"] = $kodzon;
+        $arrwaktu["nama_zon"] = $namazon;
+        $arrwaktu["tarikh_masa"] = $tarikhmasa;
+        
+        # display data in json format
+        return json_encode($arrwaktu);
+        // }
     }
 }
